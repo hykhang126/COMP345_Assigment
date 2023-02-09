5a9afbdc1e7cbc2fbe79f4af86ca7bc2d478bef2
JGambino00 <66328776+JGambino00@users.noreply.github.com>
2023-02-08 23:28:03 -0500

    Started working on player class

--- Player.h
+++ Player.h
@@ -1,3 +1,15 @@
-class Player{
+#include <list>
+#include <vector>
+#include "Territory.h"
 
+using namespace std;
+
+class Player{
+    private :
+        Hand* _hand;
+        vector<Territory*>* tCollection;
+    public :
+        list<Territory> toDefend();
+        list<Territory> toAttack();
+        void issueOrder();
 };
