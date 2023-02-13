55b7134b0547f0f2f9da10bebe5d80d9f32ec9ba
David <79176006+david-gitxyz@users.noreply.github.com>
2023-02-12 20:28:44 -0500

    [Orders] Removed unnecessary comments

--- Orders.h
+++ Orders.h
@@ -7,21 +7,6 @@
 
 using namespace std;
 
-// Specific design requirements
-// 1. All data members of user-defined class type must be of pointer type.
-// 2. All file names and the content of the files must be according to what is given in the 
-// description below.
-// 3. All different parts must be implemented in their own separate .cpp/.h file duo. All 
-// functions’ implementation must be provided in the .cpp file (i.e. no inline functions are 
-// allowed).
-// 4. All classes must implement a correct copy constructor, assignment operator, and stream 
-// insertion operator.
-// 5. Memory leaks must be avoided.
-// 6. Code must be documented using comments (user-defined classes, methods, free functions, operators).
-// 7. If you use third-party libraries that are not available in the labs and require 
-// setup/installation, you may not assume to have help using them and you are entirely responsible 
-// for their proper installation for grading purposes.
-
 /**
  * Order: User-defined class to create Order objects
  * Types of Orders: Deploy
