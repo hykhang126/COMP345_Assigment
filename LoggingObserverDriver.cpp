afbd080384805123bac9864c0afcf1f7b7b131eb
Eric Hanna <erichanna2009@hotmail.com>
2023-03-16 18:08:11 -0400

    adding driver

--- LoggingObserverDriver.cpp
+++ LoggingObserverDriver.cpp
@@ -3,10 +3,54 @@
 //
 
 #include "Command.h"
+#include "CommandProcessor.h"
+#include "Orders.h"
+
 int main() {
 
-    Command* com = new Command();
+    // Creating observer and subjects
+    LogObserver *gameLogObserver = new LogObserver;
 
-    com->SaveEffect(new string("hey"));
-    com->SaveEffect(new string("nice"));
+    Command* com = new Command(new string("test command"));
+    CommandProcessor* comProc = new CommandProcessor();
+    GameEngine* gameEngine = new GameEngine();
+    OrdersList* ordersList = new OrdersList();
+    Deploy* deployOrder = new Deploy();
+
+    // Attaching all subjects with observer
+    com->attach(gameLogObserver);
+    comProc->attach(gameLogObserver);
+    gameEngine->attach(gameLogObserver);
+    ordersList->attach(gameLogObserver);
+    deployOrder->attach(gameLogObserver);
+
+    // Performing state changes. Logs can be found in ./cmake-build-debug/gameLog.txt
+    ordersList->addOrder(deployOrder);
+    ordersList->getList().back()->execute();
+    com->SaveEffect(new string("test effect"));
+    comProc->SaveCommand(com);
+    gameEngine->isCommandValid(new  string("loadMap"));
+
+
+    // Detaching all subjects from observer
+    com->detach(gameLogObserver);
+    comProc->detach(gameLogObserver);
+    gameEngine->detach(gameLogObserver);
+    ordersList->detach(gameLogObserver);
+    deployOrder->detach(gameLogObserver);
+
+    // Deleting pointers
+    delete(gameLogObserver);
+    gameLogObserver = nullptr;
+
+    // Individual orders and commands deleted when deleting ordersList and comProc, no need to delete them twice
+    delete(comProc);
+    delete gameEngine;
+    delete ordersList;
+
+    com = nullptr;
+    comProc = nullptr;
+    gameEngine = nullptr;
+    ordersList = nullptr;
+    deployOrder = nullptr;
 };
