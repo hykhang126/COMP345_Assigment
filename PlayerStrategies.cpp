e6e23b047276cd5447261c1795ede3e0d73180c3
Eric Hanna <erichanna2009@hotmail.com>
2023-04-16 01:50:11 -0400

    Aggressive, benevolent comments

--- PlayerStrategies.cpp
+++ PlayerStrategies.cpp
@@ -165,8 +165,17 @@ void Human::issueOrder() {
 
 }
 
+/*
+ *
+ *      Aggressive class
+ *
+ */
+
+// Returns a list of territories that can be attacked
+// These are the territories that are adjacent to the territory that has the msot armies currently
 vector<Territory *> *Aggressive::toAttack() {
 
+    // First find the territory with the max armies and its index
     vector<Territory *> *defendList = p->getTerritoryCollection();
     int maxArmies = 0;
     int territoryMaxArmiesIndex = 0;
@@ -183,8 +192,10 @@ vector<Territory *> *Aggressive::toAttack() {
         iteratorIndex++;
     }
 
+    // these will be territories next to the territory with max armies
     vector<Territory *> *territoriesToAttack = new vector<Territory *>;
 
+    // then we check the adjacent territories and only add them if they are not owned by the aggressive player
     auto adjacentTerritories = defendList->at(territoryMaxArmiesIndex)->adjacentTerritory;
 
     for (Territory *adjacTerritory: *adjacentTerritories) {
@@ -194,7 +205,13 @@ vector<Territory *> *Aggressive::toAttack() {
     }
     return territoriesToAttack;
 }
-
+/**
+ *
+ * Returning a list of territories upon which the aggressive player will add armies
+ * This is simply going to be a list of 1 territory: the one that already has the maximum amount of armies
+ * But, to make it able to win, it must also be able to realize when a territory doesn't have adjacent non-owned territories anymore
+ * At which point it moves their armies to the next territory that has adjacent territories that aren't his
+ */
 vector<Territory *> *Aggressive::toDefend() {
 
     vector<Territory *> *defendList = p->getTerritoryCollection();
@@ -202,6 +219,9 @@ vector<Territory *> *Aggressive::toDefend() {
     int maxArmies = 0;
     int territoryMaxArmiesIndex = 0;
     int iteratorIndex = 0;
+    // Check all possible territories
+    // Check that the territory has adjacent territories that aren't owned
+    // If so, update the max armies, and check for max armies next iterations
     for (Territory *territory: *defendList) {
         if (*territory->getArmies() >= maxArmies) {
             for (Territory *adjacTerritory: *territory->adjacentTerritory)
@@ -214,6 +234,8 @@ vector<Territory *> *Aggressive::toDefend() {
         }
         iteratorIndex++;
     }
+    // If somehow we are still at size 0, that means that there are no attackable adjacent territories to any of our current territories
+    // So we just return the one with max army
     if (territoriesToDefend->size() == 0) {
         maxArmies = 0;
         territoryMaxArmiesIndex = 0;
@@ -229,6 +251,7 @@ vector<Territory *> *Aggressive::toDefend() {
     return territoriesToDefend;
 }
 
+// Here we deploy, do airlift if they are in our hand and do advance
 void Aggressive::issueOrder() {
     Territory *strongestTerritory = this->toDefend()->back();
 
@@ -246,6 +269,7 @@ void Aggressive::issueOrder() {
          << " their total of " << *p->getReinforcement() << " reinforcements" << endl;
 
     p->getOrdersList()->addOrder(new Deploy(p->getReinforcement(), strongestTerritory, p));
+    // Check for the territory that has the second maximumm number of armies, we will airlift from that territory to our strongest territory
     int secondMaxArmies = 0;
     while (p->hasCardInHand("airlift")) {
         cout << "Starting airlift for aggressive player " << *p->getName() << endl;
@@ -260,8 +284,6 @@ void Aggressive::issueOrder() {
             }
         }
 
-        string srcTerritory;
-        string targetTerritory;
 
 
         int iteratorIndex = 0;
@@ -290,20 +312,10 @@ void Aggressive::issueOrder() {
             }
         }
     }
-    // attacking
-//    cout
-//            << "The following territories are the aggressive " << *p->getName()
-//            << " list of territories to be defended along with how many armies they currently have: "
-//            << endl;
-//    //vector<Territory*>* defendList = this->toDefend();
-//    for (auto it = p->getTerritoryCollection()->begin(); it != p->getTerritoryCollection()->end(); ++it) {
-//        cout << *(*it)->getName() << ", Armies: " << *(*it)->getArmies() << endl;
-//    }
-
+    // Finally take care of advancing, we send all our armies
     cout << "\nThe following territories are aggressive " << *p->getName() << " list of territories to attack:" << endl;
     vector<Territory *> *attackList = this->toAttack();
     if (attackList->size() == 0) {
-
         cout << "couldn't find a suitable enemy to attack this turn for aggressive player " << *p->getName() << endl;
     } else {
         cout << *attackList->back()->getName() << " holding: " << *attackList->back()->getArmies() << " armies."
@@ -322,71 +334,83 @@ void Aggressive::issueOrder() {
     }
 }
 
+// Default  constructor, make its own player
 Aggressive::Aggressive() {
     p = new Player();
 }
 
+// Constructor with an existing player, should be more used
 Aggressive::Aggressive(Player *player) {
     this->p = player;
 }
 
+// Destructor, take care of dangling pointers
 Aggressive::~Aggressive() {
     delete p;
     p = NULL;
 }
 
-// returning default output
+// Returning Aggressive player with their name
 std::ostream &operator<<(ostream &out, const Aggressive &aggressive) {
     out << "PlayerStrategy is Aggressive with name " << *aggressive.p->getName() << endl;
     return out;
 }
 
-// copy constructor makes new default player (deep copy)
+// Copy constructor makes new aggressive player (deep copy)
 Aggressive::Aggressive(const Aggressive &agg) {
     this->p = new Player(*agg.p);
 }
-
+// Same thing for assignment operator
 Aggressive &Aggressive::operator=(const Aggressive &agg) {
     this->p = new Player(*agg.p);
     return *this;
 }
 
-// BENEVOLENT
-
+/*
+ *
+ *      Benevolent class
+ *
+ */
+// Default constructor, make new default player
 Benevolent::Benevolent() {
     p = new Player();
 }
 
+// Constructor with existing player
 Benevolent::Benevolent(Player *player) {
     this->p = player;
 }
 
+// Typical destructor
 Benevolent::~Benevolent() {
     delete p;
     p = NULL;
 }
 
-// returning default output
+// Returning benevolent type and name
 std::ostream &operator<<(ostream &out, const Benevolent &Benevolent) {
     out << "PlayerStrategy is Benevolent with name " << *Benevolent.p->getName() << endl;
     return out;
 }
 
-// copy constructor makes new default player (deep copy)
+// copy constructor makes new benevolent player (deep copy)
 Benevolent::Benevolent(const Benevolent &ben) {
     this->p = new Player(*ben.p);
 }
 
+// same thing for assignment
 Benevolent &Benevolent::operator=(const Benevolent &ben) {
     this->p = new Player(*ben.p);
     return *this;
 }
 
+// A benevolent player never attacks other territories
 vector<Territory *> *Benevolent::toAttack() {
     vector<Territory *> *territoriesToAttack = new vector<Territory *>;
     return territoriesToAttack;
 }
 
+// For this, we sort the territories by their number of armies and return it (descending order => back has the lowest number of armies)
 vector<Territory *> *Benevolent::toDefend() {
     vector<Territory *> *defendList = p->getTerritoryCollection();
     std::sort(defendList->begin(), defendList->end(), [](const auto &lhs, const auto &rhs) {
@@ -396,17 +420,23 @@ vector<Territory *> *Benevolent::toDefend() {
     return defendList;
 }
 
+// We deploy, airlift
+// We deploy 1 army at a time, and only ever airlift 1 army at a time to make sure we always focus on the weakest territories at any point in time
+// In accordance with the typical gameplay loop of Warzone
 void Benevolent::issueOrder() {
 
-
+    // Return descending sorted list of territories
     vector<Territory *> *defendList = toDefend();
     cout
             << "The following territories are your list of benevolent player " << *p->getName()
             << " territories to be defended along with how many armies they currently have: "
             << endl;
 
+    // Simulate a dictionary with two vectors
+    // Dictionary holds name of territories and how many deployed armies the benevolent player plans to do
     vector<string> territoriesName;
     vector<int> territoriesDeployedArmies;
+    // Initialize dictionary, and print current territories
     for (auto it = defendList->begin(); it != defendList->end(); ++it) {
         cout << *(*it)->getName() << ", Armies: " << *(*it)->getArmies() << endl;
         territoriesName.push_back(*(*it)->getName());
@@ -416,22 +446,26 @@ void Benevolent::issueOrder() {
          << " available armies to deploy."
          << endl;
     int *armiesToDeploy = new int(1);
-    int stackReinforcements = *p->getReinforcement();
 
-
+    // Deploy 1 army at a time until there are no armies left
     while (*p->getReinforcement()>0) {
-
+        // The weakest territory will initially be at the back
         cout << "Benevolent player deploying " << *armiesToDeploy << " armies to weakest territory "
              << *defendList->back()->getName() << endl;
 
-
+        // We deploy 1 army, so now there is 1 less reinforcement
         p->getOrdersList()->addOrder(new Deploy(armiesToDeploy, defendList->back(), p));
         p->setReinforcement(new int(*p->getReinforcement() -1));
+        // Keep track of how many deployed armies are put in that territory for future needs
+        // This is because adding an order doesn't actually update the army values, we need to keep track of the army values somewhere
         for (int i = 0; i<territoriesName.size(); i++){
             if(territoriesName.at(i) == *defendList->back()->getName()){
                 territoriesDeployedArmies.at(i)++;
             }
         }
+        // Then we can re-sort the list of territories taking into account the added number of territories (that haven't been deployed yet but will be when orderlist executes it)
+        // Find the index for the left part and add its current non-updated armies to the number of updated armies using the index
+        // Do the same for the right part
         std::sort(defendList->begin(), defendList->end(), [this, &territoriesName,&territoriesDeployedArmies](const auto &lhs, const auto &rhs) {
             for (int i = 0; i<territoriesName.size(); i++){
                 if(territoriesName.at(i) == *lhs->getName()){
@@ -444,7 +478,8 @@ void Benevolent::issueOrder() {
         });
     }
 
-
+    // Airlift simply adds 1 army from the strongest to the weakest territory
+    // Simply sort and send from front to back
     while (p->hasCardInHand("airlift")) {
         cout << "Starting airlift for benevolent player " << *p->getName() << endl;
         cout << "The following is a list of the territories benevolent " << *p->getName() << " owns: " << endl;
